229874e1259f9505a2b60153db6fc492290f08d2
Miha0809 <miha080104@gmail.com>
2024-11-04 00:00:51 +0100

    Lesson 4

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 export const Categories = () => {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            className={activeIndex === index ? "active" : ""}
+            onClick={() => setActiveIndex(index)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,37 +1,39 @@
 import { useState } from "react";
 
-export const PizzaBlock = ({ title, price }) => {
-  const [count, setCount] = useState(0);
-
-  const addToCart = () => {
-    setCount(count + 1);
-  };
+export const PizzaBlock = ({ title, imageUrl, typeIndexs, sizes, price }) => {
+  const typeNames = ["Тонке", "Традиційне"];
+  const [activeSizeIndex, setActiveSizeIndex] = useState(0);
+  const [activeDoughIndex, setActiveDoughIndex] = useState(typeIndexs[0]);
 
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {typeIndexs.map((type) => (
+            <li
+              className={activeDoughIndex === type ? "active" : ""}
+              onClick={() => setActiveDoughIndex(type)}
+            >
+              {typeNames[type]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, index) => (
+            <li
+              className={activeSizeIndex === index ? "active" : ""}
+              onClick={() => setActiveSizeIndex(index)}
+            >
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} грн.</div>
-        <button
-          className="button button--outline button--add"
-          onClick={addToCart}
-        >
+        <button className="button button--outline button--add">
           <svg
             width="12"
             height="12"
@@ -45,7 +47,7 @@ export const PizzaBlock = ({ title, price }) => {
             />
           </svg>
           <span>Добавить</span>
-          <i>{count}</i>
+          <i>0</i>
         </button>
       </div>
     </div>
